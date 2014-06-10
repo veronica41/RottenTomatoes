70228b99364b20da6e06db68c9785f0e8f93be4f
Veronica Zheng <zhengzhixing@gmail.com>
2014-06-10 01:28:11 -0700

    setup project and install AFNetworking iwth CocoaPods

---
+++ RottenTomatoes/RTAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  RTAppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Veronica Zheng on 6/10/14.
+//  Copyright (c) 2014 Veronica Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ RottenTomatoes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RottenTomatoes
+//
+//  Created by Veronica Zheng on 6/10/14.
+//  Copyright (c) 2014 Veronica Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RTAppDelegate class]));
+    }
+}
