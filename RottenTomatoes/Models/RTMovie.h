2d7bf8fdc877b548131a6ea60a6f4d8812f33571
Veronica Zheng <zhengzhixing@gmail.com>
2014-06-10 06:06:23 -0700

    add Models

---
+++ RottenTomatoes/Models/RTMovie.h
@@ -0,0 +1,18 @@
+//
+//  RTMovie.h
+//  RottenTomatoes
+//
+//  Created by Veronica Zheng on 6/10/14.
+//  Copyright (c) 2014 Veronica Zheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RTMovie : NSObject
+
+@property (nonatomic, strong) NSString * title;
+@property (nonatomic, strong) NSString * synopsis;
+@property (nonatomic, strong) NSString * posterThumbnailUrl;
+@property (nonatomic, strong) NSString * posterOriginalUrl;
+
+@end
---
+++ RottenTomatoes/Models/RTMovie.m
@@ -0,0 +1,13 @@
+//
+//  RTMovie.m
+//  RottenTomatoes
+//
+//  Created by Veronica Zheng on 6/10/14.
+//  Copyright (c) 2014 Veronica Zheng. All rights reserved.
+//
+
+#import "RTMovie.h"
+
+@implementation RTMovie
+
+@end
