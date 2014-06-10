aca11c05eb87147f85c723931898b79d8e27c0b7
Veronica Zheng <zhengzhixing@gmail.com>
2014-06-10 06:06:48 -0700

    add Constants

---
+++ RottenTomatoes/RTConstants.h
@@ -0,0 +1,35 @@
+//
+//  RTConstants.h
+//  RottenTomatoes
+//
+//  Created by Veronica Zheng on 6/10/14.
+//  Copyright (c) 2014 Veronica Zheng. All rights reserved.
+//
+
+#ifndef RottenTomatoes_Header_h
+#define RottenTomatoes_Header_h
+
+#define IPHONE_SCREEN_WIDTH         320.0
+#define IPHONE_SCREEN_HEIGHT  480.0
+
+#define MOVIES_TABLE_ROW_HEIGHT 200
+
+
+#pragma mark - strings
+
+#define BOX_OFFICE_TAB_BAR_TITLE @"Box Office"
+#define TOP_DVDS_TAB_BAR_TITLE @"Top DVDs"
+#define MOVIES_VIEW_TITLE @"Movies"
+
+
+#pragma mark - images
+
+#define BOX_OFFICE_TAB_BAR_IMAGE @"box_office_tab"
+#define TOP_DVDS_TAB_BAR_IMAGE @"top_dvds_tab"
+
+#pragma mark - urls
+
+#define BOX_OFFICE_URL @"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=g9au4hv6khv6wzvzgt55gpqs"
+#define TOP_DVDS_URL @"http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=g9au4hv6khv6wzvzgt55gpqs"
+
+#endif
